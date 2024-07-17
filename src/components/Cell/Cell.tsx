37b8de45656734905118681bd3802f6140c8e3a0
Dmitry Artemov <d-artemov@yandex-team.ru>
2024-07-18 00:05:33 +0200

    feat(Table): remove cell-content

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -10,15 +10,12 @@ import {b} from '../Table/Table.classname';
 export interface CellProps<TData> {
     cell: CellProperties<TData, unknown>;
     className?: string;
-    contentClassName?: string;
 }
 
-export const Cell = <TData,>({cell, className, contentClassName}: CellProps<TData>) => {
+export const Cell = <TData,>({cell, className}: CellProps<TData>) => {
     return (
         <td className={b('cell', getCellClassModes(cell), className)} style={getCellStyles(cell)}>
-            <div className={b('cell-content', contentClassName)}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-            </div>
+            {flexRender(cell.column.columnDef.cell, cell.getContext())}
         </td>
     );
 };
--- src/utils/renderDefaultGroupHeader.tsx
+++ src/utils/renderDefaultGroupHeader.tsx
@@ -9,15 +9,13 @@ export const renderDefaultGroupHeader = <TData,>(
     row: Row<TData>,
     getGroupTitle?: (row: Row<TData>) => React.ReactNode,
 ) => (
-    <div className={b('cell-content', b('group-title-wrapper'))}>
-        <h2 className={b('group-title')}>
-            <button onClick={row.getToggleExpandedHandler()} className={b('group-expand-button')}>
-                <ArrowToggle direction={row.getIsExpanded() ? 'bottom' : 'right'} size={16} />
-                <span className={b('group-title-content')}>
-                    <span>{getGroupTitle?.(row) ?? row.id}</span>
-                    <span className={b('group-total')}>{row.subRows.length ?? ''}</span>
-                </span>
-            </button>
-        </h2>
-    </div>
+    <h2 className={b('group-title')}>
+        <button onClick={row.getToggleExpandedHandler()} className={b('group-expand-button')}>
+            <ArrowToggle direction={row.getIsExpanded() ? 'bottom' : 'right'} size={16} />
+            <span className={b('group-title-content')}>
+                <span>{getGroupTitle?.(row) ?? row.id}</span>
+                <span className={b('group-total')}>{row.subRows.length ?? ''}</span>
+            </span>
+        </button>
+    </h2>
 );
