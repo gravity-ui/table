e07d74a837579183734cb6bd54d7341ae261c075
DanisAvko <48149690+DanisAvko@users.noreply.github.com>
2024-08-16 14:56:54 +0300

    feat(Table): aria-(colcount,colindex,rowcount,rowindex) support added (#24)

--- src/components/FooterRow/FooterRow.tsx
+++ src/components/FooterRow/FooterRow.tsx
@@ -6,7 +6,7 @@ import type {FooterCellProps} from '../FooterCell';
 import {FooterCell} from '../FooterCell';
 import {b} from '../Table/Table.classname';
 
-export interface FooterRowProps<TData, TValue> {
+export interface FooterRowProps<TData, TValue> extends React.TdHTMLAttributes<HTMLTableRowElement> {
     cellClassName: FooterCellProps<TData, TValue>['className'];
     className?: string;
     footerGroup: HeaderGroup<TData>;
@@ -16,6 +16,7 @@ export const FooterRow = <TData, TValue>({
     cellClassName,
     className,
     footerGroup,
+    ...restProps
 }: FooterRowProps<TData, TValue>) => {
     const isEmptyRow = footerGroup.headers.every((header) => !header.column.columnDef.footer);
 
@@ -24,7 +25,7 @@ export const FooterRow = <TData, TValue>({
     }
 
     return (
-        <tr className={b('footer-row', className)}>
+        <tr className={b('footer-row', className)} {...restProps}>
             {footerGroup.headers.map((header) => (
                 <FooterCell key={header.column.id} className={cellClassName} header={header} />
             ))}
