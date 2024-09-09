31ef94d52f47cdd369f5391a103bb2d15d584b50
Dmitry Artemov <d-artemov@yandex-team.ru>
2024-09-09 12:04:08 +0200

    fix(BaseTable): do not import dnd-kit if reordering is not used (#34)

--- src/components/SortableListContext/SortableListContext.tsx
+++ src/components/SortableListContext/SortableListContext.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import type {useSortable} from '@dnd-kit/sortable';
+
 import type {useSortableList} from '../../hooks';
 
-type SortableListContextValue = ReturnType<typeof useSortableList>;
+type SortableListContextValue = ReturnType<typeof useSortableList> & {
+    useSortable?: typeof useSortable;
+};
 
 export const SortableListContext = React.createContext<SortableListContextValue | undefined>(
     undefined,
