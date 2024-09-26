31a70d868ba7d9cda061f316e26eb19d4d6f1bec
tapo4ek <tapo4ek@yandex-team.ru>
2024-09-26 10:55:18 +0300

    feat(Table): removed hasChildrenColumns helper func

--- src/components/TableSettingsColumn/TableSettingsColumn.utils.ts
+++ src/components/TableSettingsColumn/TableSettingsColumn.utils.ts
@@ -1,14 +1,10 @@
 import type {Column, VisibilityState} from '@tanstack/react-table';
 
-export const hasChildrenColumns = <TData extends unknown>(
-    column: Column<TData, unknown>,
-): boolean => column.columns.length > 0;
-
 export const getIsVisible = <TData extends unknown>(
     column: Column<TData, unknown>,
     visibilityState: VisibilityState,
 ): boolean => {
-    if (hasChildrenColumns(column))
+    if (column.columns.length)
         return column.columns.some((innerColumn) => getIsVisible(innerColumn, visibilityState));
     return visibilityState[column.id] ?? true;
 };
