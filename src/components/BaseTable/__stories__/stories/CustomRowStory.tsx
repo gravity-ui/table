28bf6d28d0e65980ed4a645b9fea2301d5b0baec
Dmitry Artemov <d-artemov@yandex-team.ru>
2024-10-04 12:12:37 +0200

    fix(BaseTable): pass getIsCustomRow, groupHeaderClassName and renderCustomRowContent into BaseRow (#66)

---
+++ src/components/BaseTable/__stories__/stories/CustomRowStory.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import type {ColumnDef, Row} from '@tanstack/react-table';
+
+import {useTable} from '../../../../hooks';
+import type {BaseTableProps} from '../../BaseTable';
+import {BaseTable} from '../../BaseTable';
+import {data} from '../constants/data';
+import type {Item} from '../types';
+
+interface CustomItem {
+    isCustom: boolean;
+}
+
+type ItemOrCustom = Item | CustomItem;
+
+const dataWithCustomRow: Array<ItemOrCustom> = [...data, {isCustom: true}];
+
+const getIsCustomRow = (row: Row<ItemOrCustom>) => {
+    return 'isCustom' in row.original && row.original.isCustom;
+};
+
+const renderCustomRowContent: BaseTableProps<ItemOrCustom>['renderCustomRowContent'] = ({Cell}) => {
+    return <Cell colSpan={2}>This is a custom row</Cell>;
+};
+
+const columns: ColumnDef<ItemOrCustom>[] = [
+    {accessorKey: 'name', header: 'Name', size: 150},
+    {accessorKey: 'age', header: 'Age', size: 150},
+];
+
+export const CustomRowStory = () => {
+    const table = useTable({
+        columns,
+        data: dataWithCustomRow,
+    });
+
+    return (
+        <BaseTable
+            table={table}
+            getIsCustomRow={getIsCustomRow}
+            renderCustomRowContent={renderCustomRowContent}
+        />
+    );
+};
