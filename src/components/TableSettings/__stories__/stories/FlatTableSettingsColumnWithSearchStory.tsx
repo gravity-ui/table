a86c2771c7214689a4bf916d84696bc30c82b63d
Olga Polikashina <olga.polikashina@gmail.com>
2025-06-10 12:36:51 +0300

    fix: separate data definition from story

--- src/components/TableSettings/__stories__/stories/FlatTableSettingsColumnWithSearchStory.tsx
+++ src/components/TableSettings/__stories__/stories/FlatTableSettingsColumnWithSearchStory.tsx
@@ -6,82 +6,12 @@ import {SETTINGS_COLUMN_ID, getSettingsColumn, selectionColumn} from '../../../.
 import {useTable} from '../../../../hooks';
 import {Table} from '../../../Table/Table';
 import type {TableSettingsOptions} from '../../TableSettings';
+import type {Item} from '../constants/flatSettings';
+import {data, tableColumns} from '../constants/flatSettings';
 
 import {cnTableSettingsColumnWithSearchStory} from './TableSettingsColumnWithSearchStory.classname';
 
 import './TableSettingsColumnWithSearchStory.scss';
-
-type Item = {
-    id: string;
-    contactGroupName: string;
-    contactName: string;
-    contactPhone: string;
-    contactGroupType: string;
-    contactPriority: string;
-};
-
-const data: Item[] = [
-    {
-        id: '32',
-        contactGroupName: 'Contact Group 1',
-        contactName: 'Contact 1',
-        contactPhone: '+7 123 456 789',
-        contactGroupType: 'Tech support',
-        contactPriority: '1',
-    },
-    {
-        id: '33',
-        contactGroupName: 'Contact Group 2',
-        contactName: 'Contact 2',
-        contactPhone: '+7 123 456 789',
-        contactGroupType: 'Business support',
-        contactPriority: '1',
-    },
-    {
-        id: '34',
-
-        contactGroupName: 'Contact Group 3',
-        contactName: 'Contact 3',
-        contactPhone: '+7 123 456 789',
-        contactGroupType: 'Admin support',
-        contactPriority: '1',
-    },
-];
-
-const tableColumns: ColumnDef<Item>[] = [
-    {
-        id: 'contact_group_name',
-        accessorKey: 'contactGroupName',
-        header: 'Group',
-        minSize: 250,
-    },
-    {
-        id: 'contact_group_type',
-        accessorKey: 'contactGroupType',
-        header: 'Type',
-        minSize: 230,
-    },
-
-    {
-        id: 'contacts_group_name',
-        accessorKey: 'contactName',
-        header: 'Name',
-        minSize: 200,
-    },
-    {
-        id: 'contacts_group_phone',
-        accessorKey: 'contactPhone',
-        header: 'Phone',
-    },
-    {
-        id: 'contacts_group_priority',
-        accessorKey: 'contactPriority',
-        header: () => <span>Priority</span>,
-        meta: {
-            titleInSettings: 'Priority',
-        },
-    },
-];
 
 export const FlatTableSettingsColumnWithSearchStory = ({
     sortable,
