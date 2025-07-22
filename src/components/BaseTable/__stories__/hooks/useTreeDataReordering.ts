46a582d272cdb571f328843463606f4ae94b167d
Dmitry Artemov <d-artemov@yandex-team.ru>
2025-07-22 15:24:54 +0200

    fix: add .js extension to lodash imports

--- src/components/BaseTable/__stories__/hooks/useTreeDataReordering.ts
+++ src/components/BaseTable/__stories__/hooks/useTreeDataReordering.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import cloneDeep from 'lodash/cloneDeep';
+import cloneDeep from 'lodash/cloneDeep.js';
 
 import type {ReorderingProviderProps} from '../../../ReorderingProvider';
 import type {TreeItem} from '../constants/tree';
