1a013977e08fc0c39dcb87e06a29846e85953ee9
Radomir-Drukh <82394073+Radomir-Drukh@users.noreply.github.com>
2025-10-10 16:44:24 +0300

    Update src/components/Table/__stories__/Table.stories.tsx
    Co-authored-by: Dmitry Artemov <d-artemov@yandex-team.ru>

--- src/components/Table/__stories__/Table.stories.tsx
+++ src/components/Table/__stories__/Table.stories.tsx
@@ -2,7 +2,7 @@ import type {Meta, StoryObj} from '@storybook/react';
 
 import {Table} from '../index';
 
-import {GroupingWithSelectionStory} from './stories//GroupingWithSelectionStory';
+import {GroupingWithSelectionStory} from './stories/GroupingWithSelectionStory';
 import {DefaultStory} from './stories/DefaultStory';
 import {GroupingStory} from './stories/GroupingStory';
 import {ReorderingStory} from './stories/ReorderingStory';
